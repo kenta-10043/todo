687e4d482cc3ac91db470a59739aae8763a54c23
mori kenta <aquamoon1436@gmail.com>
2025-07-10 00:16:29 +0900

    2章終了

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -23,6 +23,20 @@ public function store(TodoRequest $request){
 
 }
 
+public function update(TodoRequest $request)
+{
+    $todo =$request ->only('content');
+    Todo::find($request->id)->update($todo);
+
+    return redirect('/')->with('message','Todoを更新しました');
+}
+
+public function destroy(Request $request)
+{
+    Todo::find($request->id)->delete();
+
+    return redirect('/')->with('message','Todoを削除しました');
+}
 
 
 }
